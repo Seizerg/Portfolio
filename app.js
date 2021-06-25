6757def685f8f6791aa623a8ba3388febdb88594
Seizerg <63910941+Seizerg@users.noreply.github.com>
2021-06-25 13:56:44 +0530

    first version

---
+++ app.js
@@ -0,0 +1,11 @@
+const express= require("express");
+const body= require("body-parser");
+const app= express();
+app.use(express.static("public"));
+app.use(body.urlencoded({extended:true}));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+})
+app.listen(3000,function(){
+  console.log("Your server is on");
+})
---
+++ public/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if ( this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+
+    }
+    else{
+      $(".navbar").removeClass("sticky");
+
+    }
+  })
+  // typing animation
+  var typed= new Typed(".typing",{
+    strings: ["Developer","UX/UI Designer","Gamer","Overthinker" ],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop: true
+  })
+  var typed= new Typed(".typing-2",{
+    strings: ["Developer","UX/UI Designer","Gamer","Overthinker" ],
+    typeSpeed: 100,
+    backSpeed:60,
+    loop: true
+  })
+  // toggle menu
+  $(".menu-btn").click(function(){
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+    $(".menu-btn").toggleClass("active");
+  });
+  //owl OwlCarousel
+$('.owl-carousel').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0:{
+        items: 1,
+        nav: false
+      },
+      600:{
+        items:2,
+        nav:false
+      },
+      1200:{
+        items:3,
+        nav:false
+      },
+      1800:{
+        items:4,
+        nav:false
+      },
+      2400:{
+        items:5,
+        nav:false
+      }
+    }
+
+});
+
+
+});
