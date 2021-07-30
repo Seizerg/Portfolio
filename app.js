e2f9e5721a22e4aeb5271ac172e1338b321a6028
Seizerg <63910941+Seizerg@users.noreply.github.com>
2021-07-30 12:47:50 +0530

    v2

--- app.js
+++ app.js
@@ -1,54 +1,88 @@
-const express= require("express");
-const mongoose= require("mongoose")
-const body= require("body-parser");
-const app= express();
-mongoose.connect("mongodb+srv://admin-sahilsinha:portfolio@cluster0.ego4z.mongodb.net/clientDB",{useNewUrlParser:true, useUnifiedTopology:true})
-const clientSchema= new mongoose.Schema({
-  name:{
-    type:String,
-    required:true
-  },
-  email:{
-    type:String,
-    required:true
-  },
-  subject:[String],
-  text:[String]
+const express = require("express");
+const mongoose = require("mongoose")
+const body = require("body-parser");
+const app = express();
+mongoose.connect("mongodb+srv://admin-sahilsinha:portfolio@cluster0.ego4z.mongodb.net/clientDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-const Client = mongoose.model("Client",clientSchema);
+const clientSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  subject: [String],
+  text: [String]
+})
+const Client = mongoose.model("Client", clientSchema);
 app.use(express.static("public"));
-app.use(body.urlencoded({extended:true}));
-app.get("/",function(req,res){
-  res.sendFile(__dirname+"/index.html");
+app.use(body.urlencoded({
+  extended: true
+}));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 })
-app.post("/",function(req,res){
-  const name= req.body.name;
-  const email=req.body.email;
-  const subject= req.body.subject;
-  const projectdetails= req.body.projectdetails;
-  const clients= new Client({
-    name:name,
-    email:email,
-    subject:subject,
-    text:projectdetails
+app.post("/", function(req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const subject = req.body.subject;
+  const projectdetails = req.body.projectdetails;
+  const clients = new Client({
+    name: name,
+    email: email,
+    subject: subject,
+    text: projectdetails
   })
-  Client.findOne({email:email},function(err,result){
-    if(!err){
-      if(!result){
-          clients.save();
-          res.redirect("/");
-      }else{
+  Client.findOne({
+    email: email
+  }, function(err, result) {
+    if (!err) {
+      if (!result) {
+        clients.save();
+        res.redirect("/");
+      } else {
         result.subject.push(subject);
         result.text.push(projectdetails);
         result.save();
-       res.redirect("/");
+        res.redirect("/");
       }
     }
   })
 
 })
-app.post("/download",function(req,res){
-  res.send("Work in progress")
+app.post("/download", function(req, res) {
+  res.sendFile(__dirname + "/public/Resume.pdf")
+})
+app.post("/certificates", (req, res) => {
+  const button = req.body.button;
+  switch (button) {
+    case "web":
+      res.sendFile(__dirname+"/public/certificates/web.pdf")
+
+      break;
+    case "python1":
+      res.sendFile(__dirname+"/public/certificates/python1.pdf")
+
+      break;
+    case "python2":
+      res.sendFile(__dirname+"/public/certificates/python2.pdf")
+
+      break;
+    case "python3":
+      res.sendFile(__dirname+"/public/certificates/python3.pdf")
+
+      break;
+    case "training":
+      res.sendFile(__dirname+"/public/certificates/training.pdf")
+
+      break;
+    default:
+
+  }
 })
 
 
@@ -59,9 +93,6 @@ app.post("/download",function(req,res){
 
 
 
-
-
-
-app.listen(process.env.PORT||3000,function(){
+app.listen(process.env.PORT || 3000, function() {
   console.log("Your server is on");
 })
--- public/script.js
+++ public/script.js
@@ -1,78 +1,94 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
-    if ( this.scrollY > 20) {
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
 
-    }
-    else{
+    } else {
       $(".navbar").removeClass("sticky");
 
     }
-    if(this.scrollY>500){
+    if (this.scrollY > 500) {
       $(".topscroll-btn").addClass("show");
-    }
-    else {
+    } else {
       $(".topscroll-btn").removeClass("show");
-    }})
-    //scrolling up function
-    $(".topscroll-btn").click(function(){
-      $("html").animate({scrollTop:9})
+    }
+  })
+  //scrolling up function
+  $(".topscroll-btn").click(function() {
+    $("html").animate({
+      scrollTop: 9
     })
+  })
   // typing animation
-  var typed= new Typed(".typing",{
-    strings: ["Developer","UI Designer","Coder","Melophile" ],
+  var typed = new Typed(".typing", {
+    strings: ["Developer", "UI Designer", "Coder", "Engineer"],
     typeSpeed: 100,
-    backSpeed:60,
+    backSpeed: 60,
     loop: true
   })
-  var typed= new Typed(".typing-2",{
-    strings: ["Developer","UI Designer","Coder","Melophile" ],
+  var typed = new Typed(".typing-2", {
+    strings: ["Developer", "UI Designer", "Coder", "Engineer"],
     typeSpeed: 100,
-    backSpeed:60,
+    backSpeed: 60,
     loop: true
   })
   // toggle menu
-  $(".menu-btn").click(function(){
+  $(".menu-btn").click(function() {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
     $(".menu-btn").toggleClass("active");
   });
-  $(".navbar .menu li a").click(function(){
+  $(".navbar .menu li a").click(function() {
     $(".navbar .menu").removeClass("active");
   })
   //owl OwlCarousel
-$('.owl-carousel').owlCarousel({
+  $('.owl-carousel').owlCarousel({
     margin: 20,
     loop: true,
     autoplayTimeOut: 2000,
     autoplayHoverPause: true,
+    nav: true,
     responsive: {
-      0:{
+      0: {
         items: 1,
-        nav: false
-      },
-      600:{
-        items:2,
         nav:false
+
       },
-      1200:{
-        items:3,
+      600: {
+        items: 2,
+          nav:false
+
+      },
+      1200: {
+        items: 3,
+          nav:false
+
+      },
+      1800: {
+        items: 4,
         nav:false
+
       },
-      1800:{
-        items:4,
-        nav:false
+      2400: {
+        items: 5,
+          nav:false
+
       },
-      2400:{
-        items:5,
+      3000:{
+        items:6,
+          nav:false
+      },
+      3600:{
+        items:7,
         nav:false
       }
     }
 
-});
-// scroll btn scripting
-$("#top-scroll").click(function(){
-  $("html body").animate({
-    scrollTop: $("#home").offset().top},2000);
+  });
+  // scroll btn scripting
+  $("#top-scroll").click(function() {
+    $("html body").animate({
+      scrollTop: $("#home").offset().top
+    }, 2000);
   });
 });
